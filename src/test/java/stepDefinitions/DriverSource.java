291eddf61fb54291fe84aa77e53a2c649942a8ec
ismail <ismailbakir082@gmail.com>
2024-02-23 00:53:37 +0100

    headless

--- src/test/java/stepDefinitions/DriverSource.java
+++ src/test/java/stepDefinitions/DriverSource.java
@@ -1,16 +1,18 @@
 package stepDefinitions;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public enum DriverSource {
     //only one instance
     INSTANCE;
     private WebDriver driver;
+    private ChromeOptions options = new ChromeOptions();
 
     public WebDriver getDriver() {
         if(driver == null) {
-            driver = new ChromeDriver();
+            options.addArguments("--headless"); //Headlees argumani eklendi
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
